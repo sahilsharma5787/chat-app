bcfc0562d51c283d476d75e4192fa4aa9206ff4a
sahilsharma5787 <sahilsh7331@gmail.com>
2024-12-13 10:53:13 +0530

    first commit

---
+++ frontend/src/constants/color.js
@@ -0,0 +1,10 @@
+export const orange = "#1c1c1c";
+export const orangeLight = "#1c1c1c";
+
+export const grayColor = "rgba(247,247,247,1)";
+export const lightBlue = "#2694ab";
+export const matBlack = "#1c1c1c";
+export const bgGradient = "rgba(247,247,247,1)";
+
+export const purple = "rgba(75,12,192,1)";
+export const purpleLight = "rgba(75,12,192,0.2)";
